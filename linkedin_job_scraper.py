54a4362732f7c53973734d0bb45ee8473ba48257
projectfetcher <calolinalindas2000@gmail.com>
2026-06-11 16:31:54 +0200

    Update linkedin_job_scraper.py

--- linkedin_job_scraper.py
+++ linkedin_job_scraper.py
@@ -63,11 +63,11 @@ WP_PASSWORD = "st8a 6mWY wqgV 0syR mB3i y5FQ"
 
 SEARCH_KEYWORDS = [
     "",
-    "engineer", "developer", "manager", "finance", "sales", "HR",
-    "doctor", "construction", "logistics", "operations", "customer service",
-    "teacher", "chef", "lawyer", "graphic designer", "production manager",
-    "petroleum", "driver", "security", "researcher", "journalist",
-    "banker", "retail", "renewable energy",
+   # "engineer", "developer", "manager", "finance", "sales", "HR",
+  #  "doctor", "construction", "logistics", "operations", "customer service",
+  #  "teacher", "chef", "lawyer", "graphic designer", "production manager",
+  #  "petroleum", "driver", "security", "researcher", "journalist",
+  #  "banker", "retail", "renewable energy",
 ]
 
 # =============================================================================
